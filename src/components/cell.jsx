ea86cb0ddfddd6687901fbf102cce37149987631
Robert King <robert.o.king@hotmail.co.uk>
2021-09-09 17:46:44 +0200

    Added: <circle> drawn in cells with a value (ie: that have been clicked on)

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -7,9 +7,17 @@ export const Cell = ({ i, j, value, cellSize, clickHandler }) => {
   }
 
   return (
-    <rect
-      x={i * cellSize} y={j * cellSize}
-      width={cellSize} height={cellSize}
-      onClick={handleClick} />
+    <g>
+      { value &&
+        <circle
+          cx={(i + 0.5) * cellSize}
+          cy={(j + 0.5) * cellSize}
+          r={cellSize * 0.4} /> }
+      <rect
+        x={i * cellSize} y={j * cellSize}
+        width={cellSize} height={cellSize}
+        onClick={handleClick} />
+      }
+    </g>
   );
 };
